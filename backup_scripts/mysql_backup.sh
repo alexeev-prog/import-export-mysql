162926fb6d46ad2309ea901571e88733107f86ed
alexeev-prog <alexeev.dev@mail.ru>
2024-12-26 21:40:51 +0700

    update bash scripts

--- backup_scripts/mysql_backup.sh
+++ backup_scripts/mysql_backup.sh
@@ -1,8 +1,9 @@
-TIMESTAMP=$(date +"%F") # получаем текущую дату
-BACKUP_DIR="<путь до директории, где будут сохраняться дампы>"
-MYSQL_USER="<имя пользователя>"
-DATABASE_NAME="<название базы данных>"
+TIMESTAMP=$(date +'%F') # получаем текущую дату
+BACKUP_DIR='<путь до директории, где будут сохраняться дампы>'
+MYSQL_USER='<имя пользователя>'
+MYSQL_PASSWORD='<пароль пользователя>'
+DATABASE_NAME='<название базы данных>'
 
-mkdir -p "$BACKUP_DIR/$TIMESTAMP" # создаем директорию
-mysqldump -u $MYSQL_USER -p $DATABASE_NAME > "$BACKUP_DIR/$TIMESTAMP/$DATABASE_NAME-$TIMESTAMP.sql" # создаем дамп
-find "$BACKUP_DIR" -type d -mtime +28 -exec rm -rf {} \; # удаление бекапов старше 4 недель
+mkdir -p '$BACKUP_DIR/$TIMESTAMP' # создаем директорию
+mysqldump -u $MYSQL_USER -p$MYSQL_PASSWORD $DATABASE_NAME > '$BACKUP_DIR/$TIMESTAMP/$DATABASE_NAME-$TIMESTAMP.sql' # создаем дамп
+find '$BACKUP_DIR;' -type d -mtime +28 -exec rm -rf {} \; # удаление бекапов старше 4 недель
